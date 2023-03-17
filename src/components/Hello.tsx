ef04795c0aeb527c3c9ad0efd758299caec37563
dirk122119 <dirk122119@gmail.com>
2023-03-17 21:38:30 +0800

    webpack config ok

---
+++ src/components/Hello.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import '../style.scss'
+export const Hello = () => (
+    <>
+     <h1>Hello World</h1>
+     <main>haha<div>hihi</div></main>
+    </>
+ 
+);
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+//   mode:'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+              },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 將 CSS 添加到 DOM 中
+          'style-loader',
+          // 解析 CSS 中的 `@import` 和 `url()`
+          'css-loader',
+          // 將 Sass 轉換為 CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js",".jsx"],
+  },
+  devServer: {
+    static: "./dist",
+    // historyApiFallback: true,
+    compress: true,
+    port: 9000,
+  },
+  output: {
+    filename: "boundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+}
+};
